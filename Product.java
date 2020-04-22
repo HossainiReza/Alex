8c22cd041cac1b464c84c7c0a256d6b30ebdbf3f
Rezaafg <rezahosseiniafg@gmail.com>
2020-04-22 23:25:52 +0200

    First

---
+++ Product.java
@@ -0,0 +1,62 @@
+
+public class Product {
+    private String name = "Coca Cola2";
+    private float price =10;
+    private int id;
+
+    public Product(String name, float price ,int id)
+    {
+        setName(name);
+        setPrice(price);
+        setId(id);
+
+  private onStock: boolean = true;
+    
+   private float price = 10;
+    public Product(String name)
+    {
+        setName(name);
+        setOnStock(true);
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+   public void setPrice(price);
+   {
+      this.price= price;
+   }    
+    public void  setId(id);
+
+  public void setsetOnStock(boolean onStock)
+   {
+      this.onStock=onStock;
+   }
+    
+    
+    public String getName()
+
+    {
+        this.id= id;
+    }
+   public boolean getOnStock()
+   {
+      return onStock;
+   }
+
+    public String getName()
+    {
+      return name;
+    }
+      public float getPrice()
+    {
+      return price;
+    }
+     public int getId()
+    {
+      return id;
+    }
+
+}
